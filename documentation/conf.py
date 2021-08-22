19e26b07bfce1c05c87233d4a4227ed4e82ba3eb
Phudoncou <zoufudun@163.com>
2021-08-22 12:11:22 +0800

    Update conf.py

--- documentation/conf.py
+++ documentation/conf.py
@@ -30,7 +30,7 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['recommonmark',
-  'sphinx_markdown_tables',
+#  'sphinx_markdown_tables',
   'sphinx.ext.autosectionlabel',
 ]
 
